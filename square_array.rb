542cfa36ac7e475407bdc1dc5095e1208d97fd0b
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-02 04:26:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   count = 0
   array.each do |index|
-    new_array[count] = index ** index
+    new_array[count] = index * index
     count += 1
   end
   new_array
